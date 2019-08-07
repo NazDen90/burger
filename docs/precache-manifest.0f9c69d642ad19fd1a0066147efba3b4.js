a7ac9c89e121573db4bf8b167386a5b0892308f1
Nazar Denysiak <nazden009@gmail.com>
2019-08-07 15:52:21 +0300

    build

---
+++ docs/precache-manifest.0f9c69d642ad19fd1a0066147efba3b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2aced3cea7ad41c34985394448e5ffc8",
+    "url": "/burger/index.html"
+  },
+  {
+    "revision": "72bd5191ae7dfe7f0bc3",
+    "url": "/burger/static/css/main.d9e81c65.chunk.css"
+  },
+  {
+    "revision": "b3bfb461373861b2aa59",
+    "url": "/burger/static/js/2.7d0acd69.chunk.js"
+  },
+  {
+    "revision": "72bd5191ae7dfe7f0bc3",
+    "url": "/burger/static/js/main.d5f64b58.chunk.js"
+  },
+  {
+    "revision": "06bc0fb602b0b05bc96a",
+    "url": "/burger/static/js/runtime~main.7e2e1d8b.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger/static/media/burger-logo.b8503d26.png"
+  }
+]);
